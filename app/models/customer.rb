0d0ae4aeb0ad9d2085ece34f19f65728cd96fa20
yinwai92 <wanyw92@gmail.com>
2015-07-07 14:40:20 +0800

    image upload s3

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,9 +2,19 @@ class Customer < ActiveRecord::Base
 	# paperclip upload setting
 	has_attached_file :avatar,
 	                :styles => { :medium => "300x300>", :thumb => "100x100>" },
-	                :default_url => "/images/:style/missing.png"
+	                :default_url => "/images/:style/missing.png",
+	                :storage => :s3,
+	                :path => "/profile-custom",
+	                :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
-	validates_attachment  :avatar, :presence => true,
+	validates_attachment  :avatar, 
 	                    :content_type => { :content_type => ["image/jpeg", "image/jpg", "image/png"] },
 	                    :size => { :in => 0..10.megabytes }
+
+	# define s3 credentials
+	def s3_credentials
+    	{:bucket => ENV['AWS_BUCKET'], 
+    	 :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
+    	 :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+  	end
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
-  config.api_key = "9b410df7f0a0817a3549d07d9c3fac1b"
-  config.secret = "825313df5c5e495a66a8829c056240f3"
+  config.api_key = ENV["SHOPIFY_API_KEY"]
+  config.secret = ENV["SHOPIFY_SHARED_SECRET"]
   config.scope = "read_orders, read_products, read_customers, write_customers"
   config.embedded_app = true
 end
