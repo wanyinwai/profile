ee7cc28af179580dab676df23ca26dd04d4e580f
yinwai92 <wanyw92@gmail.com>
2015-07-10 21:26:44 +0800

    remove script tag

--- app/models/session_storage.rb
+++ app/models/session_storage.rb
@@ -11,9 +11,9 @@ class SessionStorage
     shop = Shop.find(id)
     ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
 
-    ShopifyAPI::Base.activate_session(ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token))
-    scriptTag = ShopifyAPI::ScriptTag.create(:event => "onload", :src => "http://customer-profile.herokuapp.com/assets/#{ActionController::Base.helpers.asset_path('application.js')}")
-    scriptTag.save
+    # ShopifyAPI::Base.activate_session(ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token))
+    # scriptTag = ShopifyAPI::ScriptTag.create(:event => "onload", :src => "http://customer-profile.herokuapp.com/assets/#{ActionController::Base.helpers.asset_path('application.js')}")
+    # scriptTag.save
 
   rescue ActiveRecord::RecordNotFound
     nil
