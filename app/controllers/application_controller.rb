907de6cc7cbc2553b881a5f79837f106389955d8
yinwai92 <wanyw92@gmail.com>
2015-07-13 06:29:57 +0800

    header in application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
+  after_filter :set_access_control_headers
+
   include ShopifyApp::Controller
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  after_filter :set_access_control_headers
-
+  
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = 'http://toysinvest.com'
     headers['Access-Control-Request-Method'] = '*'
