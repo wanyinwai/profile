bcc581e4bfe137ccb46b8389ef69fadc6ee83325
yinwai92 <wanyw92@gmail.com>
2015-07-13 17:11:34 +0800

    s3 endpoint added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@ class Customer < ActiveRecord::Base
 									:storage => :s3,
 									:path => "/profilecustom/:id/:style/:filename",
 									:bucket => ENV['AWS_BUCKET'],
-									:s3_endpoint => 's3-ap-southeast-1.amazonaws.com',
+									:s3_host_name => "s3-ap-southeast-1.amazonaws.com",
 	                :s3_credentials => {
 	                  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 	                  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
