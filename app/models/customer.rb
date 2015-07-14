fb8287a39e3977fd5ff961167142095354aa4245
yinwai92 <wanyw92@gmail.com>
2015-07-14 13:53:55 +0800

    default url added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ActiveRecord::Base
 	has_attached_file :avatar,
 	                :styles => { :medium => "300x300>", :thumb => "100x100>" },
 									:storage => :s3,
-									:default_url => "profilecustom/image/default.png",
+									:default_url => "default.png",
 									:path => "profilecustom/:id/:style/:filename",
 									:bucket => ENV['AWS_BUCKET'],
 									:s3_host_name => "s3-ap-southeast-1.amazonaws.com",
@@ -11,7 +11,6 @@ class Customer < ActiveRecord::Base
 	                  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 	                  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 	            	  }
-
 
 
 	validates_attachment  :avatar,
